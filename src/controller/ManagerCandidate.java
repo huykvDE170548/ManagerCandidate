7db9107689af88ad15aef620db2de5b021191d3b
Van Huy <kieuvanhuy813@gmail.com>
2023-09-15 09:05:56 +0700

    First Commit

---
+++ src/controller/ManagerCandidate.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import model.CandidateList;
+import view.Menu;
+
+/**
+ *
+ * @author This PC
+ */
+public class ManagerCandidate extends Menu<String>{
+    
+    
+    CandidateList candidateList= new CandidateList();   
+    public ManagerCandidate(String title, String[] s){
+        super(title, s);
+    }
+    
+    @Override
+    public void execute(int n) {
+        switch (n) {
+            case 1:
+                CandidateList.createCandidate(0);
+                break;
+            case 2:
+                CandidateList.createCandidate(1);
+                break;
+            case 3:
+                CandidateList.createCandidate(2);
+                break;
+            case 4:
+                CandidateList.searchCandidate();
+                break;
+            case 5:
+                System.exit(0);
+        }
+    }   
+    
+}
